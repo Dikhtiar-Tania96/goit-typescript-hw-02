a209b53b6b3345bef2389cc0669fe148607942ff
Dikhtiar Tania <you@example.com>
2024-07-14 16:51:01 +0300

    new info

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -10,12 +10,14 @@ interface ImageModalProps {
 }
 
 Modal.setAppElement("#root");
+
+
 const ImageModal: React.FC <ImageModalProps> = ({ image, onClose }) => {
   return (
     <div>
       <Modal overlayClassName={css.overlay} isOpen={true} onRequestClose={onClose} className={css.modalWindow}>
         <button  onClick={onClose} className={css.modalBtn}>X</button>
-        <img src={image.urls.small} alt={image.alt_description} className={css.modalImage} width={600} height={500} />
+        <img src={image.urls.regular} alt={image.alt_description} className={css.modalImage} width={600} height={500} />
         <p>{image.description}</p>
         <p>Author: {image.user.name}</p>
         <p>Likes: {image.likes}</p>      
